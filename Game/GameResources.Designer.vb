a02676938512645c023761a35496d714fe348216
djambot <cacyk@SHAMIN>
2026-01-26 12:10:02 +0000

    main menu sprite fix + new button

--- Game/GameResources.Designer.vb
+++ Game/GameResources.Designer.vb
@@ -97,6 +97,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend Shared ReadOnly Property btnVOLTARAOMENU_export() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("btnVOLTARAOMENU-export", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend Shared ReadOnly Property character_sprites() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("character_sprites", resourceCulture)
