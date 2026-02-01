3bcd2ad0d56d37a5f3eb1bca7ddd592d78e766b5
anfidaniil <anfi.daniil@gmail.com>
2026-02-01 12:15:50 +0000

    Added ImmovableComponent to buffs in order to stop enemies from pushing buffs around. Changed FIXED_DT to lower the amount of physics updates, leading to smoother FPS

--- Game/Form1.vb
+++ Game/Form1.vb
@@ -8,7 +8,7 @@
     Private fps As Integer = 0
     Private lastCheck As Date
     Private accumulator As Double = 0
-    Private Const FIXED_DT As Double = 0.01 ' 100 Hz
+    Private Const FIXED_DT As Double = 0.02 ' 100 Hz
 
     Private isSpaceDown As Boolean = False
     Private isMouseDown As Boolean = False
