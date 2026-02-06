bd8e19dd003fa6695117e35a438ea5e174cd8d0f
djambot <cacyk@SHAMIN>
2026-02-06 10:53:50 +0000

    new map

--- Game/GameResources.Designer.vb
+++ Game/GameResources.Designer.vb
@@ -473,5 +473,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend Shared ReadOnly Property zabornijnyy() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("zabornijnyy", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Class
 End Namespace
